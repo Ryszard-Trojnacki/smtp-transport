aa9edbba2f5764cb17690e3516dee20fb7fa0a93
Ryszard-Trojnacki <ryszard.trojnacki@gmail.com>
2015-11-04 14:29:08 +0100

    Added SMTP server library.

--- src/main/java/pl/rtprog/smtptransport/Main.java
+++ src/main/java/pl/rtprog/smtptransport/Main.java
@@ -1,5 +1,10 @@
 package pl.rtprog.smtptransport;
 
+import org.subethamail.smtp.MessageContext;
+import org.subethamail.smtp.MessageHandler;
+import org.subethamail.smtp.MessageHandlerFactory;
+import org.subethamail.smtp.server.SMTPServer;
+
 /**
  * Startup class.
  * 
@@ -7,6 +12,11 @@ package pl.rtprog.smtptransport;
  */
 public class Main {
 	public static void main(String[] args) {
-		
+		SMTPServer smtpd=new SMTPServer(new MessageHandlerFactory() {
+			@Override
+			public MessageHandler create(MessageContext ctx) {
+				return null;
+			}
+		});
 	}
 }
